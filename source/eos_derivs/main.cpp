9599f1b240ee148ed64b35d5dcce1ad7c2c3f28a
biboyd <brendanboyd16@gmail.com>
2025-08-11 18:47:11 -0400

    get omegadot involved

--- source/eos_derivs/main.cpp
+++ source/eos_derivs/main.cpp
@@ -37,7 +37,7 @@ void main_main()
         pltfile.pop_back();
     }
 
-    std::string outfile = "convwork." +
+    std::string outfile = "eosderiv." +
         std::filesystem::path(pltfile).filename().string();
 
 
@@ -63,6 +63,7 @@ void main_main()
     int dens_comp = get_dens_index(var_names_pf);
     int temp_comp = get_temp_index(var_names_pf);
     int spec_comp = get_spec_index(var_names_pf);
+    int omegadot_comp = get_omegadot_index(var_names_pf);
     int radvel_comp = get_radvel_index(var_names_pf); //account for no radvel and calc on the fly
     // create the variable names we will derive and store in the output
     // file
@@ -72,34 +73,6 @@ void main_main()
     gvarnames.push_back("eta_e");
     gvarnames.push_back("therm_term_h");
     gvarnames.push_back("sigma");
-
-    // interpret the boundary conditions
-    BCRec bcr_default;
-    Array<int,AMREX_SPACEDIM> is_periodic{AMREX_D_DECL(0,0,0)};
-    IntVect ng(1);
-    for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
-        if (idim < ndims) {
-            bcr_default.setLo(idim, BCType::hoextrapcc);
-            bcr_default.setHi(idim, BCType::hoextrapcc);
-        } else {
-            bcr_default.setLo(idim, BCType::int_dir);
-            bcr_default.setHi(idim, BCType::int_dir);
-            is_periodic[idim] = 1;
-            ng[idim] = 0;
-        }
-    }
-
-    // get center
-
-    Array<Real, AMREX_SPACEDIM> center;
-    auto const probLo = pf.probLo();
-    auto const probHi = pf.probHi();
-
-    for (int idim = 0; idim < AMREX_SPACEDIM; ++idim){
-        center[idim] = 0.5_rt * (probHi[idim] - probLo[idim]);
-    }
-
-    // we need both T and P constructed with ghost cells
 
     Vector<MultiFab> gmf(nlevs);
     Vector<Geometry> geom;
@@ -123,78 +96,6 @@ void main_main()
         // fill the density and temperature mfs with ghost cells
         // we also need all of the species
 
-        MultiFab temp_mf(pf.boxArray(ilev), pf.DistributionMap(ilev), 1, ng);
-        MultiFab dens_mf(pf.boxArray(ilev), pf.DistributionMap(ilev), 1, ng);
-        MultiFab species_mf(pf.boxArray(ilev), pf.DistributionMap(ilev), NumSpec, ng);
-
-        if (ilev == 0) {
-
-            // temperature
-            {
-                MultiFab smf = pf.get(ilev, var_names_pf[temp_comp]);
-                FillPatchSingleLevel(temp_mf, ng, Real(0.0), {&smf}, {Real(0.0)},
-                                     0, 0, 1, vargeom, physbcf, 0);
-            }
-
-            // density 
-            {
-                MultiFab smf = pf.get(ilev, var_names_pf[dens_comp]);
-                FillPatchSingleLevel(dens_mf, ng, Real(0.0), {&smf}, {Real(0.0)},
-                                     0, 0, 1, vargeom, physbcf, 0);
-            }
-
-            // species
-            {
-                for (int n = 0; n < NumSpec; ++n) {
-                    MultiFab smf = pf.get(ilev, var_names_pf[spec_comp+n]);
-                    FillPatchSingleLevel(species_mf, ng, Real(0.0), {&smf}, {Real(0.0)},
-                                         0, n, 1, vargeom, physbcf, 0);
-                }
-            }
-
-        } else {
-            auto* mapper = (Interpolater*)(&cell_cons_interp);
-
-            IntVect ratio(pf.refRatio(ilev-1));
-            for (int idim = ndims; idim < AMREX_SPACEDIM; ++idim) {
-                ratio[idim] = 1;
-            }
-
-            Geometry cgeom(pf.probDomain(ilev-1), RealBox(pf.probLo(),pf.probHi()),
-                           pf.coordSys(), is_per);
-            PhysBCFunct<GpuBndryFuncFab<FabFillNoOp>> cphysbcf
-                (cgeom, bcr, GpuBndryFuncFab<FabFillNoOp>(FabFillNoOp{}));
-
-            // temperature
-            {
-                MultiFab cmf = pf.get(ilev-1, var_names_pf[temp_comp]);
-                MultiFab fmf = pf.get(ilev  , var_names_pf[temp_comp]);
-                FillPatchTwoLevels(temp_mf, ng, Real(0.0), {&cmf}, {Real(0.0)},
-                                   {&fmf}, {Real(0.0)}, 0, 0, 1, cgeom, vargeom,
-                                   cphysbcf, 0, physbcf, 0, ratio, mapper, bcr, 0);
-            }
-
-            // density 
-            {
-                MultiFab cmf = pf.get(ilev-1, var_names_pf[dens_comp]);
-                MultiFab fmf = pf.get(ilev  , var_names_pf[dens_comp]);
-                FillPatchTwoLevels(dens_mf, ng, Real(0.0), {&cmf}, {Real(0.0)},
-                                   {&fmf}, {Real(0.0)}, 0, 0, 1, cgeom, vargeom,
-                                   cphysbcf, 0, physbcf, 0, ratio, mapper, bcr, 0);
-            }
-
-            // species
-            {
-                for (int n = 0; n < NumSpec; ++n) {
-                    MultiFab cmf = pf.get(ilev-1, var_names_pf[spec_comp+n]);
-                    MultiFab fmf = pf.get(ilev  , var_names_pf[spec_comp+n]);
-                    FillPatchTwoLevels(species_mf, ng, Real(0.0), {&cmf}, {Real(0.0)},
-                                       {&fmf}, {Real(0.0)}, 0, n, 1, cgeom, vargeom,
-                                       cphysbcf, 0, physbcf, 0, ratio, mapper, bcr, 0);
-                }
-            }
-
-        }
 
         auto const& dx = pf.cellSize(ilev);
 
@@ -209,31 +110,19 @@ void main_main()
             // output storage
             auto const& ga = gmf[ilev].array(mfi);
 
-            // temperature and density with ghost cells
-            auto const& rho = dens_mf.const_array(mfi);
-            auto const& T = temp_mf.const_array(mfi);
-            auto const& X = species_mf.const_array(mfi);
 
             // all of the data without ghost cells
             const auto& fab = lev_data_mf.array(mfi);
 
             amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE (int i, int j, int k)
             {
-
-                // calc position 
-                Real xpos, ypos, zpos, rpos;
-                xpos = probLo[0] + dx[0] * (Real(i) + 0.5_rt) - center[0];
-                ypos = probLo[1] + dx[1] * (Real(j) + 0.5_rt) - center[1];
-                zpos = probLo[2] + dx[2] * (Real(k) + 0.5_rt) - center[2];
-                rpos = std::sqrt(xpos*xpos + ypos*ypos + zpos*zpos);
-
-                // calc Flux of electrons
+                // calc eos
                 eos_t eos_state;
 
-                eos_state.rho = rho(i,j,k);
-                eos_state.T = T(i,j,k);
+                eos_state.rho = fab(i,j,k,dens_comp);
+                eos_state.T = fab(i,j,k,temp_comp);
                 for (int n = 0; n < NumSpec; ++n) {
-                    eos_state.xn[n] = X(i,j,k,n);
+                    eos_state.xn[n] = fab(i,j,k,spec_comp+n);
                 }
                 eos(eos_input_rt, eos_state);
             	eos_xderivs_t eos_xderivs = composition_derivatives(eos_state);
@@ -241,7 +130,7 @@ void main_main()
                 //calc sum dhdx. See xhi in S in maestro eqn's
                 Real therm_term = 0.;
                 for (int n = 0; n < NumSpec; ++n) {
-                    therm_term = eos_xderivs.dhdx[n] * omegadot[n];
+                    therm_term = eos_xderivs.dhdx[n] * fab(i,j,k,omegadot_comp+n);
                 }
 
 				//calc sigma = p_T/(rho cp p_rho)
